878a42e1c5e36901dd10ec331cedf76e27bf6f13
DimKaHUI <wet75787@tqosi.com>
2018-09-06 21:28:48 +0300

    Created Assembly analizer

---
+++ Assets/_MapEditor/Scripts/AssemplyAnalizer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets._MapEditor.Scripts
+{
+    class AssemplyAnalizer
+    {
+        public static GameType GetTypeTree(Type typeBase)
+        {
+            return CreateTree(new GameType(typeBase));
+        }
+
+        private static GameType CreateTree(GameType typeBase)
+        {
+            IEnumerable<Type> list = Assembly.GetAssembly(typeBase.Type).GetTypes().Where(type => (type.IsSubclassOf(typeBase.Type) && type.BaseType == typeBase.Type));
+
+            foreach (var type in list)
+            {
+                GameType child = CreateTree(new GameType(type));
+                typeBase.Add(child);
+            }
+
+            return typeBase;
+        }
+    }
+}
---
+++ Assets/_MapEditor/Scripts/GameType.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets._MapEditor.Scripts
+{
+    public class GameType
+    {
+        private Type _type;
+
+        private List<GameType> ChildGameTypes;
+
+        public GameType(Type type)
+        {
+            _type = type;
+            ChildGameTypes = new List<GameType>();
+        }
+
+        public GameType this[int i]
+        {
+            get { return ChildGameTypes[i]; }
+        }
+
+        public void Add(GameType type)
+        {
+            ChildGameTypes.Add(type);
+        }
+
+        public Type Type => _type;
+
+        public override string ToString()
+        {
+            if (_type.FullName != null) return _type.FullName;
+            return "No name";
+        }
+
+        public int Length => ChildGameTypes.Count;
+    }
+}
