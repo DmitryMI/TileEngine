debc1e17ff73b404a18148560267142fe1c2f27e
DimKaHUI <wet75787@tqosi.com>
2018-09-07 20:54:06 +0300

    Item sorting order implemented

--- Assets/Scripts/Controllers/TileController.cs
+++ Assets/Scripts/Controllers/TileController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Assets.Scripts.Objects;
+using Assets.Scripts.Objects.Item;
 using UnityEngine;
 using UnityEngine.Networking;
 
@@ -14,6 +15,8 @@ namespace Assets.Scripts.Controllers
     {
 
         private List<TileObject>[,] _objects;
+
+        private ItemStackCollection _itemStackCollection;
 
         private Vector2Int _mapSize;
         private bool _allocationFinished;
@@ -43,20 +46,34 @@ namespace Assets.Scripts.Controllers
         [Server]
         public void AddObject(int x, int y, TileObject obj)
         {
-            if(WasLoaded && ServerController.IsCellInBounds(x, y))
+            if (WasLoaded && ServerController.IsCellInBounds(x, y))
+            {
                 _objects[x, y].Add(obj);
+
+                Item item = obj as Item;
+                if(item)
+                    _itemStackCollection.Add(item, new Vector2Int(x, y));
+            }
 
             if (!WasLoaded)
             {
                 Debug.Log("Detected attempt to registrate object before launching TileController. This can cause malfunctions");
             }
+
+            
         }
 
         [Server]
         public void RemoveObject(int x, int y, TileObject obj)
         {
             if (WasLoaded && ServerController.IsCellInBounds(x, y))
+            {
                 _objects[x, y].Remove(obj);
+
+                Item item = obj as Item;
+                if (item)
+                    _itemStackCollection.Remove(item, new Vector2Int(x, y));
+            }
         }
 
         [Server]
@@ -85,6 +102,23 @@ namespace Assets.Scripts.Controllers
             return null;
         }
 
+        public void FindAll<T>(int x, int y, IList<T> list) where T : TileObject
+        {
+            TileObject[] objects = GetObjects(x, y);
+
+            if (objects == null)
+                return;
+
+            foreach (var obj in objects)
+            {
+                var find = obj as T;
+                if (find != null)
+                {
+                    list.Add(find);
+                }
+            }
+        }
+
         private IEnumerator WaitForAsyncAllocation()
         {
             while(_allocationTask.IsCompleted)
@@ -95,6 +129,7 @@ namespace Assets.Scripts.Controllers
 
         private void AllocateMemory()
         {
+            _itemStackCollection = new ItemStackCollection();
             _allocationTask = new Task(AllocateMemoryAsync, TaskCreationOptions.LongRunning);
             _allocationTask.Start();
 
@@ -111,6 +146,36 @@ namespace Assets.Scripts.Controllers
                 {
                     _objects[x, y] = new List<TileObject>(10);
                 }
+            }
+        }
+
+        private void Update()
+        {
+            if (WasLoaded && ServerController.Ready)
+            {
+                SortItemStacks();
+            }
+        }
+
+        private void SortItemStacks()
+        {
+            
+            foreach (var stack in _itemStackCollection)
+            {
+                SortItemStack(stack);
+            }
+        }
+
+        private void SortItemStack(ItemStack stack)
+        {
+            stack.SortStack();
+
+            int currentSortingOrder = 0;
+
+            for (int i = 0; i < stack.Length; i++)
+            {
+                currentSortingOrder += stack[i].LayersNeeded;
+                stack[i].SpriteRenderer.sortingOrder = currentSortingOrder;
             }
         }
     }
