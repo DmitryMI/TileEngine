68e24de93a2cb731c94ce0ad48a73c06b06ee5ec
DmitryMI <wet75787@tqosi.com>
2019-06-03 21:01:32 +0300

    New interfaces introduced

---
+++ Assets/Scripts/GameMechanics/ILightInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.GameMechanics
+{
+    public interface ILightInfo
+    {
+
+        ICellPositionProvider PositionProvider { get; }
+
+        Color LightColor { get; }
+
+        float Range { get; }
+
+        float Decrement { get; }
+
+        float InitialIntensity { get; }
+
+    }
+}
