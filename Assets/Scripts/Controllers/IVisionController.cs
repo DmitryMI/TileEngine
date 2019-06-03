04ea57b9255e689785f25eaf80f31d12c46d646a
DmitryMI <wet75787@tqosi.com>
2019-06-03 16:20:52 +0300

    VisionController interface created

--- Assets/Scripts/Controllers/IVisionController.cs
+++ Assets/Scripts/Controllers/IVisionController.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Assets.Scripts.GameMechanics;
+using UnityEngine;
 
 namespace Assets.Scripts.Controllers
 {
-    interface IVisionController
+    public interface IVisionController
     {
+        bool IsCellVisible(int x, int y);
+
+        bool VisionProcessingEnabled { get; }
+
+        void SetCameraPosition(Vector2 planeWorldPosition);
+
+        void SetBlock(int x, int y);
+
+        [Obsolete("VisionMask is not present in the game.")]
+        VisionMask GetMask(int x, int y);
+
+        void SetLightSource(LightSourceInfo info);
     }
 }
