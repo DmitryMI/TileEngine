bad95d3dcfffd3ef4e19cab367264b9b1a916c81
DmitryMI <wet75787@tqosi.com>
2019-06-03 21:06:58 +0300

    Utils added

---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class Utils
+    {
+        public const float PerLayerSpace = 0.1f;
+        public const float LayerOrderSpace = 0.001f;
+
+        public float LayerToZ(string layer, int order)
+        {
+            var sortingLayers = SortingLayer.layers;
+
+            int index;
+            for (index = 0; index < sortingLayers.Length; index++)
+            {
+                if (sortingLayers[index].name == layer)
+                {
+                    float layerZ = PerLayerSpace * index;
+                    float nextLayerZ = layerZ + PerLayerSpace;
+                    float orderSpace = layerZ + order * LayerOrderSpace;
+
+                    if (orderSpace >= nextLayerZ)
+                    {
+                        Debug.LogWarning("Object reached another layer");
+                    }
+
+                    return orderSpace;
+                }
+            }
+
+            return 0;
+        }
+
+    }
+}
