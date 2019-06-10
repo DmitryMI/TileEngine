d1b995b4a77836cf6effd8094aa9205873907d1b
DmitryMI <wet75787@tqosi.com>
2019-06-10 22:41:36 +0300

    IBuff interface added

---
+++ Assets/Scripts/GameMechanics/BuffSystem/IBuff.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.GameMechanics.BuffSystem
+{
+    public interface IBuff
+    {
+        SpriteSet BuffIcon { get; }
+        bool IsIconVisible { get; }
+
+        float SpeedMultiplier { get; }
+    }
+}
