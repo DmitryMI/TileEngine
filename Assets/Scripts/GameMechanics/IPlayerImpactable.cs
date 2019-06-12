606198afa5c8a84fd9347169ef175f9d8c129393
DmitryMI <wet75787@tqosi.com>
2019-06-12 12:01:46 +0300

    interface created

---
+++ Assets/Scripts/GameMechanics/IPlayerImpactable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assets.Scripts.Objects.Item;
+using UnityEngine;
+
+namespace Assets.Scripts.GameMechanics
+{
+    interface IPlayerImpactable
+    {
+        // ReSharper disable once InconsistentNaming
+        GameObject gameObject { get; }
+
+        void ImpactItemClient(Item item, Intent intent, ImpactTarget impactTarget);
+
+        void ImpactItemServer(Item item, Intent intent, ImpactTarget impactTarget);
+    }
+}
